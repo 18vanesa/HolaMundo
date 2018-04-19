d92fd22feeb807295f934f35d0084b64f69c8a26
18vanesa <38521784+18vanesa@users.noreply.github.com>
2018-04-19 07:37:20 +0000

    Update ExamenDeVanesaAlmenara.java

--- ExamenDeVanesaAlmenara/src/examenvanesa/ExamenDeVanesaAlmenara.java
+++ ExamenDeVanesaAlmenara/src/examenvanesa/ExamenDeVanesaAlmenara.java
@@ -4,6 +4,8 @@ public class ExamenDeVanesaAlmenara {
 
 	public static void main(String[] args) {
 		System.out.println("La fecha de hoy es: 19/04/2018");
+	//ejercicio2 realizado
+	//ejercicio3 realizado
 
 	}
 
